f8c8256b82956eba06cdc864e86461fa00c8e68d
lehoangkhoi01 <59761477+lehoangkhoi01@users.noreply.github.com>
2021-10-16 23:51:48 +0700

    Initialize project

---
+++ ChildCareSystem/Areas/Identity/Data/ChildCareSystemContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChildCareSystem.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChildCareSystem.Data
+{
+    public class ChildCareSystemContext : IdentityDbContext<ChildCareSystemUser>
+    {
+        public ChildCareSystemContext(DbContextOptions<ChildCareSystemContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<ChildCareSystemUser>(entity =>
+            {
+                entity.ToTable(name: "Users");
+            });
+            builder.Entity<IdentityRole>(entity =>
+            {
+                entity.ToTable(name: "Roles");
+            });
+            builder.Entity<IdentityUserRole<string>>(entity =>
+            {
+                entity.ToTable(name: "UserRoles");
+            });
+            builder.Entity<IdentityUserClaim<string>>(entity =>
+            {
+                entity.ToTable(name: "UserClaims");
+            });
+            builder.Entity<IdentityUserLogin<string>>(entity =>
+            {
+                entity.ToTable(name: "UserLogins");
+            });
+            builder.Entity<IdentityRoleClaim<string>>(entity =>
+            {
+                entity.ToTable(name: "RoleClaims");
+            });
+
+        }
+    }
+}
---
+++ ChildCareSystem/Data/ContextSeed.cs
@@ -0,0 +1,20 @@
+﻿using ChildCareSystem.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChildCareSystem.Data
+{
+    public static class ContextSeed
+    {
+        public static async Task SeedRolesAsync(UserManager<ChildCareSystemUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Seed Roles
+            await roleManager.CreateAsync(new IdentityRole(Enums.Role.Customer.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Enums.Role.Admin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Enums.Role.Staff.ToString()));
+        }
+    }
+}
